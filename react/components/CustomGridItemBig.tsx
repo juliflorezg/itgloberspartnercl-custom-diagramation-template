48040840b91a7983466934263ced9da155617003
juliflorezg <forezjuli08@hotmail.com>
2022-12-05 09:09:46 -0500

    feat: custom-grid, add CustomGrid child components

---
+++ react/components/CustomGridItemBig.tsx
@@ -0,0 +1,15 @@
+import React, {ReactNode} from 'react';
+import styles from './styles.css'
+
+type Props = {
+  element: ReactNode
+}
+
+const CustomGridItemBig = ({element}: Props) => {
+  return (<div className={styles['grid__item--big']}>
+    {element}
+  </div>)
+}
+
+export default CustomGridItemBig
+
---
+++ react/components/CustomGridItemSmall.tsx
@@ -0,0 +1,17 @@
+import React, {ReactNode} from 'react';
+import styles from './styles.css'
+
+type Props = {
+  elementOne: ReactNode
+  elementTwo: ReactNode
+}
+
+const CustomGridItemSmall = ({elementOne, elementTwo}: Props) => {
+  return (
+  <div className={styles['grid__item--small']}>
+    {elementOne}
+    {elementTwo}
+  </div>)
+}
+
+export default CustomGridItemSmall
